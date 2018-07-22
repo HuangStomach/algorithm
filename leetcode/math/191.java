15bcad569ea2af1e8269cfafc0c1b145e20a1423
huangStoamch <nxmbest@qq.com>
2018-07-22 12:15:46 +0800

    leetcode 13 191

---
+++ leetcode/math/191.java
@@ -0,0 +1,33 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += n & 1;
+            n = n >> 1;
+        }
+        return count;
+    }
+}
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += n & 1;
+            n = n >> 1;
+        }
+        return count;
+    }
+}
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += n & 1;
+            n = n >> 1;
+        }
+        return count;
+    }
+}

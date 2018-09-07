4e0bca13c047fd0eeb523fa5eefb9d52f57b2bb9
huangStoamch <nxmbest@qq.com>
2018-09-07 21:17:54 +0800

    continue 3.2

--- algs4/Search/3.2/BST.java
+++ algs4/Search/3.2/BST.java
@@ -15,9 +15,36 @@ public class BST<Key extends Comparable<Key>, Value> {
             return size(root);
         }
 
-        public int size(Node node) {
-            if (x == null) return 0;
-            else return x.N;
+        private int size(Node node) {
+            if (node == null) return 0;
+            else return node.N;
+        }
+
+        public Value get(Key key) {
+            return get(root, key);
+        }
+
+        private Value get(Node node, Key key) {
+            if (node == null) return null;
+            int cmp = key.compareTo(node.key);
+            if (cmp < 0) return get(node.left, key);
+            else if (cmp > 0) return get(node.right, key);
+            else return node.val;
+        }
+
+        public void put(Key key, Value val) {
+            root = put(root, key, val);
+        }
+
+        private Node put(Node node, Key key, Value val) {
+            if (node == null) return new Node(key, val, 1);
+            int cmp = key.compareTo(node.key);
+            if (cmp < 0) node.left = put(node.left, key, val);
+            else if (cmp > 0) node.right = put(node.right, key, val);
+            else node.val = val;
+
+            node.N = size(node.left) + size(node.right) + 1;
+            return node;
         }
     }
 }

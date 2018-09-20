dcdf7d1fad6194059561f7c4e8301f67622b1c19
huangStoamch <nxmbest@qq.com>
2018-09-20 21:50:36 +0800

    习题补充 3.2.28

--- algs4/Exercises/3.2/3.2.28.java
+++ algs4/Exercises/3.2/3.2.28.java
@@ -1,125 +1,185 @@
 import edu.princeton.cs.algs4.*;
 
-public class BinarySearchST<Key extends Comparable<Key>, Value> {
-    private Key[] keys;
-    private Value[] vals;
-    private int N;
-    private Key recentKey;
-    private int recent;
-
-    public BinarySearchST() {
-        this(5);
-    }
-
-    public BinarySearchST(int capacity) {
-        keys = (Key[]) new Comparable[capacity];
-        vals = (Value[]) new Object[capacity];
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+    private Node recent;
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int N;
+        public Node (Key key, Value val, int N) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
     }
 
     public int size() {
-        return N;
+        return size(root);
     }
 
-    public boolean isEmpty() {
-        return N == 0;
+    private int size(Node node) {
+        if (node == null) return 0;
+        else return node.N;
+    }
+
+    public int avgCompares() {
+        return length(root, 0) / size() + 1;
+    }
+
+    private int length(Node node, int l) {
+        if (node == null) return l;
+        l++;
+        int left = length(node.left, l);
+        int right = length(node.right, l);
+        return left + right;
     }
 
     public Value get(Key key) {
-        if (isEmpty()) return null;
-        if (key.compareTo(recentKey) == 0) return vals[recent];
+        if (recent.key.compareTo(key) == 0) return recent.val;
+        return get(root, key);
+    }
 
-        int i = rank(key);
-        if (i < N && keys[i].compareTo(key) == 0) {
-            recentKey = key;
-            recent = i;
-            return vals[i];
+    private Value get(Node node, Key key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) return get(node.left, key);
+        else if (cmp > 0) return get(node.right, key);
+        else {
+            recent = node;
+            return node.val;
         }
-        else return null;
     }
 
     public void put(Key key, Value val) {
-        if (key.compareTo(recentKey) == 0) {
-            vals[recent] = val;
+        if (recent.key.compareTo(key) == 0) {
+            recent.val = val;
             return;
         }
+        root = put(root, key, val);
+    }
 
-        int i = rank(key);
-        if (i < N && keys[i].compareTo(key) == 0) {
-            vals[i] = val;
-            recentKey = key;
-            recent = i;
-            return;
+    private Node put(Node node, Key key, Value val) {
+        if (node == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) node.left = put(node.left, key, val);
+        else if (cmp > 0) node.right = put(node.right, key, val);
+        else {
+            recent = node;
+            node.val = val;
         }
 
-        for (int j = N; j > i; j--) {
-            keys[j] = keys[j - 1];
-            vals[j] = vals[j - 1];
-        }
-        keys[i] = key;
-        vals[i] = val;
-        recentKey = key;
-        recent = i;
-        N++;
-        resize();
+        node.N = size(node.left) + size(node.right) + 1;
+        return node;
     }
 
     public Key min() {
-        return keys[0];
+        return min(root).key;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) return node;
+        return min(node.left);
     }
 
     public Key max() {
-        return keys[N - 1];
+        return max(root).key;
+    }
+
+    private Node max(Node node) {
+        if (node.right == null) return node;
+        return min(node.right);
+    }
+
+    public Key floor(Key key) {
+        Node node = floor(root, key);
+        if (node == null) return null;
+        return node.key;
+    }
+
+    public Node floor(Node node, Key key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp == 0) return node;
+        else if (cmp < 0) return floor(node.left, key);
+        Node t = floor(node.right, key);
+        if (t != null) return t;
+        else return node;
     }
 
     public Key select(int k) {
-        return keys[k];
+        return select(root, k).key;
     }
 
-    public Key ceiling(Key key) {
-        int i = rank(key);
-        return keys[i];
+    private Node select(Node node, int k) {
+        if (node == null) return null;
+        int t = size(node.left);
+        if (t > k) return select(node.left, k);
+        else if (t < k) return select(node.right, k - t - 1);
+        else return node;
     }
-
-    // public Key floor(Key key)
-
-    // public Key delete(Key key)
 
     public int rank(Key key) {
-        int low = 0;
-        int high = N - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int cmp = key.compareTo(keys[mid]);
-            if (cmp < 0) high = mid - 1;
-            else if (cmp > 0) low = mid + 1;
-            else return mid;
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node node) {
+        if (node == null) return 0;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) return rank(key, node.left);
+        else if (cmp > 0) return 1 + size(node.left) + rank(key, node.right);
+        else return size(node.left);
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) return node.right;
+        node.left = deleteMin(node.left);
+        node.N = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, Key key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) node.left = delete(node.left, key);
+        else if (cmp > 0) node.right = delete(node.right, key);
+        else {
+            if (node.right == null) return node.left;
+            if (node.left == null) return node.right;
+            Node t = node;
+            node = min(t.right);
+            node.right = deleteMin(t.right);
+            node.left = t.left;
         }
-        return low;
+        node.N = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public Iterable<Key> keys() {
+        return keys(min(), max());
     }
 
     public Iterable<Key> keys(Key low, Key high) {
         Queue<Key> q = new Queue<Key>();
-        for (int i = rank(low); i < rank(high); i++) {
-            q.enqueue(keys[i]);
-            if (ceiling(high) != null) {
-                q.enqueue(keys[rank(high)]);
-            }
-        }
+        keys(root, q, low, high);
         return q;
     }
 
-    private void resize() {
-        if (N <= keys.length / 2) return;
-        int length = keys.length * 2;
-        Key[] keys = (Key[]) new Comparable[length];
-        Value[] vals = (Value[]) new Object[length];
-
-        for (int i = 0; i < N; i ++) {
-            keys[i] = this.keys[i];
-            vals[i] = this.vals[i];
-        }
-
-        this.keys = keys;
-        this.vals = vals;
+    private void keys(Node node, Queue<Key> q, Key low, Key high) {
+        if (node == null) return;
+        int cmpLow = low.compareTo(node.key);
+        int cmpHigh = high.compareTo(node.key);
+        if (cmpLow < 0) keys(node.left, q, low, high);
+        if (cmpLow <= 0 && cmpHigh >= 0) q.enqueue(node.key);
+        if (cmpHigh > 0) keys(node.right, q, low, high);
     }
 }

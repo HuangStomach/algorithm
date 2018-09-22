d62c03ce2111baae892dc65f770767f0d8205d50
huangStoamch <nxmbest@qq.com>
2018-09-22 21:45:53 +0800

    习题补充 3.2.27

--- algs4/Exercises/3.2/3.2.27.java
+++ algs4/Exercises/3.2/3.2.27.java
@@ -11,12 +11,6 @@ class FrequencyCounter {
             if (!st.contains(word)) st.put(word, 1);
             else st.put(word, st.get(word) + 1);
         }
-
-        String max = " ";
-        st.put(max, 0);
-        for (String word : st.keys()) {
-            if (st.get(word) > st.get(max)) max = word;
-        }
         long free = start - runtime.freeMemory(); // 剩余内存 现在
 
         System.out.println("占用内存: " + free);

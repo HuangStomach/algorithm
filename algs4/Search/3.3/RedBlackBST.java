3898d2afb0f9ca352f383f36f547b69445476281
huangStoamch <nxmbest@qq.com>
2018-10-01 21:18:58 +0800

    continue 3.3

--- algs4/Search/3.3/RedBlackBST.java
+++ algs4/Search/3.3/RedBlackBST.java
@@ -18,8 +18,70 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) return 0;
+        else return node.N;
+    }
+
     private boolean isRed(Node x) {
         if (x == null) return false;
         return x.color = RED;
+    }
+
+    private void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+        root.color = BLACK;
+    }
+
+    private Node put(Node h, Key key, Value val) {
+        if (h == null) return new Node(key, val, 1, RED);
+
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) h.left = put(h.left, key, val);
+        else if (cmp > 0) h.right = put(h.right, key, val);
+        else h.val = val;
+
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+
+        h.N = 1 + size(h.left) + size(h.right);
+        return h;
     }
 }

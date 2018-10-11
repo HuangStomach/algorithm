609003d202ed8dd508c499bc00327c3854535f62
HuangStomach <nxmbest@qq.com>
2018-10-11 21:10:29 +0800

    习题至 3.3.33

---
+++ algs4/3.Search/3.3/Exercises/3.3.33.java
@@ -0,0 +1,235 @@
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+    private Node root;
+
+    private class Node {
+        Key key;
+        Value val;
+        Node left;
+        Node right;
+        int N;
+        boolean color;
+
+        Node(Key key, Value val, int N, boolean color) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+            this.color = color;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) return 0;
+        else return node.N;
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+    private void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node node, Key key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) return get(node.left, key);
+        else if (cmp > 0) return get(node.right, key);
+        else return node.val;
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+        root.color = BLACK;
+    }
+
+    private Node put(Node h, Key key, Value val) {
+        if (h == null) return new Node(key, val, 1, RED);
+
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) h.left = put(h.left, key, val);
+        else if (cmp > 0) h.right = put(h.right, key, val);
+        else h.val = val;
+
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+
+        h.N = 1 + size(h.left) + size(h.right);
+        return h;
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) return node;
+        return min(node.left);
+    }
+
+    public Key max() {
+        return max(root).key;
+    }
+
+    private Node max(Node node) {
+        if (node.right == null) return node;
+        return max(node.right);
+    }
+
+    public Key floor(Key key) {
+        Node node = floor(root, key);
+        if (node == null) return null;
+        return node.key;
+    }
+
+    public Node floor(Node node, Key key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp == 0) return node;
+        else if (cmp < 0) return floor(node.left, key);
+        Node t = floor(node.right, key);
+        if (t != null) return t;
+        else return node;
+    }
+
+    public Key ceiling(Key key) {
+        Node node = floor(root, key);
+        if (node == null) return null;
+        return node.key;
+    }
+
+    public Node ceiling(Node node, Key key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp == 0) return node;
+        else if (cmp > 0) return ceiling(node.right, key);
+        Node t = ceiling(node.left, key);
+        if (t != null) return t;
+        else return node;
+    }
+
+    public Key select(int k) {
+        return select(root, k).key;
+    }
+
+    private Node select(Node node, int k) {
+        if (node == null) return null;
+        int t = size(node.left);
+        if (t > k) return select(node.left, k);
+        else if (t < k) return select(node.right, k - t - 1);
+        else return node;
+    }
+
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node node) {
+        if (node == null) return 0;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) return rank(key, node.left);
+        else if (cmp > 0) return 1 + size(node.left) + rank(key, node.right);
+        else return size(node.left);
+    }
+
+    private boolean is23() {
+        is23(root);
+    }
+
+    private boolean is23(Node h) {
+        if (h == null) return true;
+        if (isRed(h.right)) return false;
+        if (h != root && isRed(h.right) && isRed(h.left)) return false;
+        return is23(h.right) && is23(h.left);
+    }
+    
+    private boolean isBalanced() { 
+        int black = 0;     // number of black links on path from root to min
+        Node h = root;
+        while (h != null) {
+            if (!isRed(h)) black++;
+            h = h.left;
+        }
+        return isBalanced(root, black);
+    }
+
+    // does every path from the root to a leaf have the given number of black links?
+    private boolean isBalanced(Node h, int black) {
+        if (h == null) return black == 0;
+        if (!isRed(h)) black--;
+        return isBalanced(h.left, black) && isBalanced(h.right, black);
+    }
+
+    private boolean isBinaryTree(Node node) {
+        if (node == null) return true;
+        int count = 1;
+        if (node.left != null) count += node.left.N;
+        if (node.right != null) count += node.right.N;
+
+        return node.N == count && isBinaryTree(node.left) && isBinaryTree(node.right);
+    }
+
+    private boolean isOrdered(Node node, Key min, Key max) {
+        if (node == null) return true;
+        if (node.key.compareTo(min) < 0 || node.key.compareTo(max) > 0) return false;
+        return isOrdered(node.left, min, node.key) && isOrdered(node.right, node.key, max);
+    }
+
+    private boolean hasNoDuplicates(Node node) {
+        if (node == null) return true;
+        if (node.key.compareTo(node.left.key) == 0 || node.key.compareTo(node.right.key) == 0 
+        || node.left.key.compareTo(node.right.key) == 0) return false;
+        return hasNoDuplicates(node.left) && hasNoDuplicates(node.right);
+    }
+
+    private boolean isBST(Node node) {
+        if (!isBinaryTree(node)) return false;
+        if (!isOrdered(node, min(), max())) return false;
+        if (!hasNoDuplicates(node)) return false;
+        return true;
+    }
+
+    public boolean isRedBlackBST() {
+        return isBST() && isBalanced() && is23();
+    }
+}

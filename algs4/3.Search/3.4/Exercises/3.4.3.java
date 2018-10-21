60670d4f1615573d831b1fa43f2cb8eba56e0b2c
HuangStomach <nxmbest@qq.com>
2018-10-21 11:19:13 +0800

    习题至 3.4.4

--- algs4/3.Search/3.4/Exercises/3.4.3.java
+++ algs4/3.Search/3.4/Exercises/3.4.3.java
@@ -54,73 +54,73 @@ class SeparateChainingHashST<Key, Value> {
         }
         if (N > 0 && N <= 2 * M) resize(M / 2);
     }
-}
 
-class SequentialSearchST<Key, Value> {
-    private int N;
-    private Node first;
-    private class Node {
-        Key key;
-        Value val;
-        Node next;
-        int index;
+    private class SequentialSearchST<Key, Value> {
+        private int N;
+        private Node first;
+        private class Node {
+            Key key;
+            Value val;
+            Node next;
+            int index;
 
-        public Node(Key key, Value val, Node next, int index) {
-            this.key = key;
-            this.val = val;
-            this.next = next;
-            this.index = index;
-        }
-    }
-
-    public Value get(Key key) {
-        for (Node x = first; x != null; x = x.next) {
-            if (key.equals(x.key)) return x.val;
-        }
-        return null;
-    }
-
-    public void put(Key key, Value val, int index) {
-        for (Node x = first; x != null; x = x.next) {
-            if (key.equals(x.key)) {
-                x.val = val;
-                return;
+            public Node(Key key, Value val, Node next, int index) {
+                this.key = key;
+                this.val = val;
+                this.next = next;
+                this.index = index;
             }
         }
-        first = new Node(key, val, first, index);
-        N++;
-    }
 
-    public Key delete(Key key) {
-        Node prev = null;
-        for (Node x = first; x != null; prev = x, x = x.next) {
-            if (key.equals(x.key)) {
-                prev.next = x.next;
-                N--;
-                return key;
+        public Value get(Key key) {
+            for (Node x = first; x != null; x = x.next) {
+                if (key.equals(x.key)) return x.val;
+            }
+            return null;
+        }
+
+        public void put(Key key, Value val, int index) {
+            for (Node x = first; x != null; x = x.next) {
+                if (key.equals(x.key)) {
+                    x.val = val;
+                    return;
+                }
+            }
+            first = new Node(key, val, first, index);
+            N++;
+        }
+
+        public Key delete(Key key) {
+            Node prev = null;
+            for (Node x = first; x != null; prev = x, x = x.next) {
+                if (key.equals(x.key)) {
+                    prev.next = x.next;
+                    N--;
+                    return key;
+                }
+            }
+            return null;
+        }
+
+        public void deleteBelow(int k) {
+            if (first == null) return;
+            Node node = first;
+            while (node != null) {
+                if (node.index <= k) delete(node.key);
+                else node = node.next;
             }
         }
-        return null;
-    }
 
-    public void deleteBelow(int k) {
-        if (first == null) return;
-        Node node = first;
-        while (node != null) {
-            if (node.index <= k) delete(node.key);
-            else node = node.next;
+        public int size() {
+            return N;
         }
-    }
 
-    public int size() {
-        return N;
-    }
-
-    public Iterable<Key> keys() {
-        List<Key> list = new ArrayList<Key>();
-        for (Node x = first; x != null; x = x.next) {
-            list.add(x.key);
+        public Iterable<Key> keys() {
+            List<Key> list = new ArrayList<Key>();
+            for (Node x = first; x != null; x = x.next) {
+                list.add(x.key);
+            }
+            return list;
         }
-        return list;
     }
 }
---
+++ algs4/3.Search/3.4/Exercises/3.4.4.java
@@ -0,0 +1,60 @@
+import edu.princeton.cs.algs4.*;
+
+class SeparateChainingHashST<Key, Value> {
+    private int N;
+    private int M;
+    private int a;
+    private SequentialSearchST<Key, Value>[] st;
+
+    public SeparateChainingHashST(int M, int a) {
+        this.M = M;
+        this.a = a;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+        for (int i = 0; i < M; i++) {
+            st[i] = new SequentialSearchST();
+        }
+    }
+
+    private int hash(Key key, int k) {
+        return (a * k) % M;
+    }
+
+    public Value get(Key key, int k) {
+        return (Value) st[hash(key, k)].get(key);
+    }
+
+    public void put(Key key, Value val, int k) {
+        st[hash(key, k)].put(key, val);
+        N = 0;
+        for (int i = 0; i < M; i++) {
+            if (st[i] != null) N += st[i].size();
+        }
+    }
+
+    public boolean prefect() {
+        int O = 0;
+        for (int i = 0; i < M; i++) {
+            if (st[i] != null && st[i].size() > 0) O++;
+        }
+        return O == N;
+    }
+
+    public static void main(String[] args) {
+        String test = "S E A R C H X M P L";
+        String[] keys = test.split("\\s+");
+        int n = keys.length;
+
+        for (int m = 11; m <= 100; m++) {
+            for (int i = 1; i <= 100; i++) {
+                SeparateChainingHashST<String, Boolean> st = new SeparateChainingHashST(m, i);
+                for (int j = 1; j <= n; j++) {
+                    st.put(keys[j - 1], true, j);
+                }
+                if (st.prefect()) {
+                    System.out.println("a: " + i + " M: " + m);
+                    return;
+                }
+            }
+        }
+    }
+}

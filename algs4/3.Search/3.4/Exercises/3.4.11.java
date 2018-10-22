8b10dab12453957353a259eaa7571fb37799dc76
HuangStomach <nxmbest@qq.com>
2018-10-22 21:23:10 +0800

    习题至 3.4.11

--- algs4/3.Search/3.4/Exercises/3.4.11.java
+++ algs4/3.Search/3.4/Exercises/3.4.11.java
@@ -18,15 +18,34 @@ class SeparateChainingHashST<Key, Value> {
         return (11 * index) % M;
     }
 
-    public Value get(Key key) {
-        return (Value) st[hash(key)].get(key);
+    private void resize(int cap) {
+        SeparateChainingHashST<Key, Value> t = new SeparateChainingHashST(cap);
+        for (int i = 0; i < M; i++) {
+            if (st[i] == null || st[i].size() == 0) continue;
+            for (Key key : st[i].keys()) {
+                t.put(key, st[i].get(key));
+            }
+        }
+        this.st = t.st;
+        this.M = t.M;
     }
 
     public void put(Key key, Value val) {
+        if (N * 2 >= M) resize(2 * M);
         st[hash(key)].put(key, val);
         N = 0;
         for (int i = 0; i < M; i++) {
             if (st[i] != null) N += st[i].size();
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < M; i++) {
+            if (st[i] == null || st[i].size() == 0) continue;
+            for (Key key: st[i].keys()) {
+                System.out.print(key + " ");
+            }
+            System.out.println();
         }
     }
 
@@ -35,14 +54,11 @@ class SeparateChainingHashST<Key, Value> {
         String[] keys = test.split("\\s+");
         int n = keys.length;
         
-        SeparateChainingHashST<String, Boolean> sta = new SeparateChainingHashST(16);
+        SeparateChainingHashST<String, Boolean> st = new SeparateChainingHashST(4);
         for (int i = 0; i < n; i++) {
-            sta.put(keys[i], true);
+            st.put(keys[i], true);
         }
-        
-        SeparateChainingHashST<String, Boolean> stb = new SeparateChainingHashST(16);
-        for (int i = 0; i < n; i++) {
-            stb.put(keys[i], true);
-        }
+
+        st.print();
     }
 }

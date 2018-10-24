61074ac414425e14cae9f601446967dda3f0fd00
HuangStomach <nxmbest@qq.com>
2018-10-24 21:14:23 +0800

    习题至 3.4.26

---
+++ algs4/3.Search/3.4/Exercises/3.4.26.java
@@ -0,0 +1,83 @@
+import edu.princeton.cs.algs4.*;
+
+public class LinearProbingHashST<Key, Value> {
+    private int N;
+    private int M;
+    private Key[] keys;
+    private Value[] vals;
+
+    public LinearProbingHashST() {
+        this(16);
+    }
+
+    public LinearProbingHashST(int M) {
+        this.M = M;
+        keys = (Key[]) new Object[this.M];
+        vals = (Value[]) new Object[this.M];
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    private void resize(int cap) {
+        LinearProbingHashST<Key, Value> t = new LinearProbingHashST(cap);
+        for (int i = 0; i < M; i++) {
+            if (keys[i] != null) t.put(keys[i], vals[i]);
+        }
+        this.keys = t.keys;
+        this.vals = t.vals;
+        this.M = t.M;
+    }
+
+    public void put(Key key, Value val) {
+        if (N >= M / 2) resize(2 * M);
+
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                vals[i] = val;
+                return;
+            }
+        }
+        keys[i] = key;
+        vals[i] = val;
+        N++;
+    }
+    
+    public Value get(Key key) {
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) return vals[i];
+        }
+        return null;
+    }
+
+    public void delete(Key key) {
+        if (!contains(key)) return;
+        int i = hash(key);
+
+        while (!key.equals(keys[i])) {
+            i = (i + 1) % M;
+        }
+
+        keys[i] = null;
+        vals[i] = null;
+
+        i = (i + 1) % M;
+        N--;
+        if (N > 0 && N == M / 8) resize(M / 2);
+        else if (keys[i] != null) resize(M);
+    }
+
+    public boolean contains(Key key) {
+        if (key == null) return false;
+        return get(key) != null;
+    }
+    
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < M; i++)
+            if (keys[i] != null) queue.enqueue(keys[i]);
+        return queue;
+    }
+}

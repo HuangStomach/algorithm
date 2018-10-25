4ff958ddd7601580c3fc18c1235e6eb9050f2e16
HuangStomach <nxmbest@qq.com>
2018-10-25 21:18:01 +0800

    习题至 3.4.30

---
+++ algs4/3.Search/3.4/Exercises/3.4.30.java
@@ -0,0 +1,78 @@
+import edu.princeton.cs.algs4.*;
+
+public class SeparateChainingHashST<Key, Value> {
+    private int N;
+    private int M;
+    private SequentialSearchST<Key, Value>[] st;
+
+    public SeparateChainingHashST() {
+        this(997);
+    }
+
+    public SeparateChainingHashST(int M) {
+        this.M = M;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+        for (int i = 0; i < M; i++) {
+            st[i] = new SequentialSearchST();
+        }
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    private void resize(int cap) {
+        SeparateChainingHashST<Key, Value> t = new SeparateChainingHashST(cap);
+        for (int i = 0; i < M; i++) {
+            if (st[i] == null || st[i].size() == 0) continue;
+            for (Key key : st[i].keys()) {
+                t.put(key, st[i].get(key));
+            }
+        }
+        this.st = t.st;
+        this.M = t.M;
+    }
+
+    public Value get(Key key) {
+        return (Value) st[hash(key)].get(key);
+    }
+
+    public void put(Key key, Value val) {
+        if (N >= 8 * M) resize(2 * M);
+        st[hash(key)].put(key, val);
+        N = 0;
+        for (int i = 0; i < M; i++) {
+            if (st[i] != null) N += st[i].size();
+        }
+    }
+
+    public void delete(Key key) {
+        int i = hash(key);
+        if (st[i].contains(key)) N--;
+        st[i].delete(key);
+        
+        if (N > 0 && N <= 2 * M) resize(M / 2);
+    }
+    
+    public boolean contains(Key key) {
+        if (key == null) return false;
+        return get(key) != null;
+    }
+    
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < M; i++) {
+            for (Key key : st[i].keys())
+                queue.enqueue(key);
+        }
+        return queue;
+    }
+
+    public double square() {
+        double value = 0.0;
+        for (int i = 0; i < M; i++) {
+            value += Math.pow(st[i].size() - N / M, 2);
+        }
+        return value * (M / N);
+    }
+}

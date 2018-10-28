f2fd8d1ad5bfd23a6dc92cd2ec01d2eb06977516
HuangStomach <nxmbest@qq.com>
2018-10-28 22:31:28 +0800

    continue 3.5

---
+++ algs4/3.Search/3.5/LookupCSV.java
@@ -0,0 +1,24 @@
+import edu.princeton.cs.algs4.*;
+
+public class LookupCSV {
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int keyField = Integer.parseInt(args[1]);
+        int valField = Integer.parseInt(args[2]);
+        ST<String, String> st = new ST<String, String>();
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] tokens = line.split(",");
+            String key = tokens[keyField];
+            String val = tokens[valField];
+            st.put(key, val);
+        }
+
+        while (!StdIn.isEmpty()) {
+            String query = StdIn.readString();
+            if (st.contains(query)) {
+                System.out.println(st.get(query));
+            }
+        }
+    }
+}

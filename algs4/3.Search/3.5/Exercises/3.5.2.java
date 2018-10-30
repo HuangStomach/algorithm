207995dc0c7f003db352bca26ffc08e1340c9727
HuangStomach <nxmbest@qq.com>
2018-10-30 20:56:30 +0800

    习题至 3.5.2

---
+++ algs4/3.Search/3.5/Exercises/3.5.2.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class SequentialSearchST<Key, Value> {
+    private int N;
+    private Node first;
+    private class Node {
+        Key key;
+        Node next;
+
+        public Node(Key key, Node next) {
+            this.key = key;
+            this.next = next;
+        }
+    }
+
+    public boolean get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) return true;
+        }
+        return null;
+    }
+
+    public void put(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) return;
+        }
+        first = new Node(key, first);
+        N++;
+    }
+
+    public Key delete(Key key) {
+        Node prev = null;
+        for (Node x = first; x != null; prev = x, x = x.next) {
+            if (key.equals(x.key)) {
+                prev.next = x.next;
+                N--;
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public Iterable<Key> keys() {
+        List<Key> list = new ArrayList<Key>();
+        for (Node x = first; x != null; x = x.next) {
+            list.add(x.key);
+        }
+        return list;
+    }
+}

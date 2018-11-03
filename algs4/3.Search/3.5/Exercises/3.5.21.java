c4f87c8106bc5683c64b31cbdc8cca63ab0a86e4
HuangStomach <nxmbest@qq.com>
2018-11-03 23:25:40 +0800

    习题至 3.5.21

---
+++ algs4/3.Search/3.5/Exercises/3.5.21.java
@@ -0,0 +1,37 @@
+import edu.princeton.cs.algs4.*;
+
+class InvertedConcordance {
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] words = in.readAllStrings();
+        ST<String, SET<Integer>> st = new ST<String, SET<Integer>>();
+
+        // build up concordance
+        for (int i = 0; i < words.length; i++) {
+            String s = words[i];
+            if (!st.contains(s)) {
+                st.put(s, new SET<Integer>());
+            }
+            SET<Integer> set = st.get(s);
+            set.add(i);
+        }
+        System.out.println("Finished building concordance");
+        invertedConcordance(st);
+    }
+
+    public static void invertedConcordance(ST<String, SET<Integer>> st) {
+        RedBlackBST<Integer, String> bst = new RedBlackBST();
+
+        for (String key: st.keys()) {
+            for (int index: st.get(key)) {
+                bst.put(index, key);
+            }
+        }
+
+        while (bst.size() > 0) {
+            System.out.print(bst.get(bst.min()) + " ");
+            bst.deleteMin();
+        }
+    }
+}

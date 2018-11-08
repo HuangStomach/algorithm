279b30a21be72508046c58c0f90dc855a0c8ab8e
HuangStomach <nxmbest@qq.com>
2018-11-08 21:38:57 +0800

    continue 4.1

--- algs4/4.Graphs/4.1/Graph.java
+++ algs4/4.Graphs/4.1/Graph.java
@@ -5,6 +5,39 @@ class Graph {
     private int E;
     private Bag<Interger>[] adj;
 
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adv[v] = new Bag<Integer>();
+        }
+    }
+    
+    public Graph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public int V() { return V; }
+
+    public int E() { return E; }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
     public String toString() {
         String s = V + " vertices, " + E + " edges \n";
         for (int v = 0; v < V; v++) {

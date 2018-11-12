1e8902515ad490567b9c39dc8a7f1bef1ab43f09
HuangStomach <nxmbest@qq.com>
2018-11-12 22:29:54 +0800

    continue 4.1

---
+++ algs4/4.Graphs/4.1/Cycle.java
@@ -0,0 +1,22 @@
+public class Cycle {
+    private boolean[] marked;
+    private boolean hashCycle;
+    public Cycle(Graph G) {
+        marked = new boolean[G.V()];
+        for (int s = 0; s < G.V(); s++) {
+            if (!marked[s]) dfs(G, s, s); 
+        }
+    }
+
+    private void dfs(Graph G, int v, int u) {
+        marked[v] = true;
+        for (int w: G.adj(v)) {
+            if (!marked[w]) dfs(G, w, v);
+            else if (w != u) hashCycle = true;
+        }
+    }
+
+    public boolean hashCycle() {
+        return hashCycle;
+    }
+}

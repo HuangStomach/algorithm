cc704c2d3912151835b376b32e9dd9cc4a4c7c54
HuangStomach <nxmbest@qq.com>
2018-11-13 21:26:19 +0800

    continue 4.1

---
+++ algs4/4.Graphs/4.1/SymbolGraph.java
@@ -0,0 +1,63 @@
+import edu.princeton.cs.algs4.*;
+
+public class SymbolGraph {
+    private ST<String, Integer> st;
+    private String[] keys;
+    private Graph G;
+
+    public SymbolGraph(String stream, String sp) {
+        st = new ST<String, Integer>();
+        In in = new In(stream);
+        while (in.hasNextLine()) {
+            String[] a = in.readLine().split(sp);
+            for (int i = 0; i < a.length; i++) {
+                if (!st.contains(a[i])) st.put(a[i], st.size());
+            }
+        }
+
+        keys = new String[st.size()];
+        for (String name: st.keys()) {
+            keys[st.get(name)] = name;
+        }
+
+        G = new Graph(st.size());
+        in = new In(Stream);
+        while (in.hasNextLine()) {
+            String[] a = in.readLine().split(sp);
+            for (int i = 0; i < a.length; i++) {
+                G.addEdge(v, st.get(a[i]));
+            }
+        }
+    }
+
+    public boolean contains(String s) {
+        return st.contains(s);
+    }
+
+    public int index(String s) {
+        return st.get(s);
+    }
+
+    public String name(int v) {
+        return keys[v];
+    }
+
+    public Graph G() {
+        return G;
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+        STring delim = args[1];
+        SymbolGraph sg = new SymbolGraph(filename, delim);
+
+        Graph G = sg.G();
+
+        while (StdIn.hasNextLine()) {
+            String source = StdIn.readLine();
+            for (int w: G.adj(sg.index(source))) {
+                System.out.println(" " + sg.name(w));
+            }
+        }
+    }
+}

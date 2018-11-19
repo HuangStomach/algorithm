d45c6853af5cf9c76215991921acedf3aa09d439
HuangStomach <nxmbest@qq.com>
2018-11-19 19:55:19 +0800

    习题至 4.1.13

---
+++ algs4/4.Graphs/4.1/Exercises/4.1.13.java
@@ -0,0 +1,54 @@
+import java.util.Queue;
+
+import edu.princeton.cs.algs4.*;
+
+public class BreathFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private int s;
+    private int count;
+
+    public BreathFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        bfs(G, s);
+    }
+
+    private void bfs(Graph G, int s) {
+        Queue<Integer> queue = new Queue<Integer>();
+        marked[s] = true;
+        queue.enqueue(s);
+        while (queue.size() > 0) {
+            int v = queue.dequeue();
+            for (int w: G.adj(v)) {
+                if (marked[i]) continue;
+                edgeTo[w] = v;
+                marked[w] = true;
+                queue.enqueue(w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int w) {
+        return marked[w];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int x = v; x != s; x = edgeTo[x]) {
+            stack.push(x);
+        }
+        stack.push(s);
+        return stack;
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public int distTo(int v) {
+        return pathTo(v).size();
+    }
+}

7142faa22a2005e44e6a8624e2854685ecbbcd69
HuangStomach <nxmbest@qq.com>
2018-11-20 21:40:07 +0800

    习题至 4.1.16

---
+++ algs4/4.Graphs/4.1/Exercises/4.1.16.java
@@ -0,0 +1,40 @@
+import edu.princeton.cs.algs4.*;
+
+class GraphProperties {
+    int[] eccentricitys;
+    int diameter = Integer.MIN_VALUE;
+    int radius = Integer.MAX_VALUE;
+    int center;
+    Graph G;
+    GraphProperties(Graph G) {
+        CC cc = new CC(G);
+        if (cc.count() > 1) throw new Exception("Error");
+        eccentricitys = new int[G.V()];
+        this.G = G;
+        for (int i = 0; i < G.V(); i++) {
+            BreathFirstPaths bfs = new BreathFirstPaths(this.G, i);
+            for (int j = 0; i < G.V(); i++) {
+                eccentricitys[i] = Math.max(eccentricitys[i], bfs.pathTo(j).size());
+            }
+            diameter = Math.max(diameter, eccentricitys[i]);
+            radius = Math.min(radius, eccentricitys[i]);
+            if (radius == eccentricitys[i]) center = i;
+        }
+    }
+
+    public int eccentricity(int v) {
+        return eccentricitys[v];
+    }
+
+    public int diameter() {
+        return diameter;
+    }
+
+    public int radius() {
+        return radius;
+    }
+
+    public int center() {
+        return center;
+    }
+}

48d139d73f1f3462ba394d23095116124416ecce
HuangStomach <nxmbest@qq.com>
2018-11-29 21:55:29 +0800

    continue 4.2

---
+++ algs4/4.Graphs/4.2/DirectedDFS.java
@@ -0,0 +1,44 @@
+import edu.princeton.cs.algs4.*;
+
+public class DirectedDFS {
+    private boolean[] marked;
+
+    public DirectedDFS(Digraph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    public DirectedDFS(Digraph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        for (int s: sources) {
+            if (!marked[s]) dfs(G, s);
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        for (int w: G.adj(v)) {
+            if (!marked[w]) dfs(G, w);
+        }
+    }
+
+    public boolean marked(int v) {
+        return marked(v);
+    }
+
+    public static void main(String[] args) {
+        Digraph G = new Digraph(new In(args[0]));
+
+        Bag<Integer> sources = new Bag<Integer>();
+        for (int i = 1; i < args.length; i++) {
+            sources.add(Integer.parseInt(args[i]));
+        }
+
+        DirectedDFS reachable = new DirectedDFS(G, sources);
+
+        for (int v = 0; v < G.V(); v++) {
+            if (reachable.marked(v)) System.out.println(v + "");
+        }
+        System.out.println();
+    }
+}

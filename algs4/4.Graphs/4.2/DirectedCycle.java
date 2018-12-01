de07f28150a7e17ddd94d49e45810ee1684203ce
HuangStomach <nxmbest@qq.com>
2018-12-01 13:23:28 +0800

    continue 4.2

---
+++ algs4/4.Graphs/4.2/DirectedCycle.java
@@ -0,0 +1,46 @@
+import edu.princeton.cs.algs4.*;
+
+public class DirectedCycle {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private Stack<Integer> cycle;
+    private boolean[] onStack;
+
+    public DirectedCycle(Digraph G) {
+        onStack = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        marked = new boolean[G.V()];
+        for (int i = 0; i < G.V(); i++) {
+            if (!marked[i]) dfs(G, i);
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        onStack[v] = true;
+        marked[v] = true;
+        for (int w: G.adj(v)) {
+            if (this.hasCycle()) return;
+            else if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+            else if (onStack[w]) {
+                cycle = new Stack<Integer>();
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+                cycle.push(v);
+            }
+        }
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+}

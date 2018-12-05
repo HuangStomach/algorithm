77584ded70ebe5d91b4efc134d3d386f8f863fe7
HuangStomach <nxmbest@qq.com>
2018-12-05 21:17:08 +0800

    习题至 4.2.5

---
+++ algs4/4.Graphs/4.2/Exercises/4.2.5.java
@@ -0,0 +1,59 @@
+import edu.princeton.cs.algs4.*;
+
+class Digraph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Digraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public Digraph(Digraph G) {
+        this.V = G.V();
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < this.V; v++) {
+            for (int w: G.adj(v)) {
+                addEdge(v, w);
+            }
+        }
+    }
+
+    public int V() { return V; }
+
+    public int E() { return E; }
+
+    public void addEdge(int v, int w) {
+        if (hasEdge(v, w) || v == w) return;
+        
+        adj[v].add(w);
+        E++;
+    }
+
+    public boolean hasEdge(int v, int w) {
+        for (int u: adj[v]) {
+            if (w == u) return true;
+        }
+        return false;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public Digraph reserve() {
+        Digraph R = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w: adj(v)) {
+                R.addEdge(w, v);
+            }
+        }
+        return R;
+    }
+}

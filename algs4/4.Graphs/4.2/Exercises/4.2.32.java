c247ae6667ee6c3564ace93303db5bc169f5557c
HuangStomach <nxmbest@qq.com>
2018-12-13 21:32:30 +0800

    begin 4.3

--- algs4/4.Graphs/4.2/Exercises/4.2.32.java
+++ algs4/4.Graphs/4.2/Exercises/4.2.32.java
@@ -2,28 +2,15 @@ import java.util.Random;
 
 import edu.princeton.cs.algs4.*;
 
-class RandomSimpleDigraph {
+class ErdosRenyiDigraph {
     public static void main(String[] args) {
         int v = Integer.parseInt(args[0]);
         int e = Integer.parseInt(args[1]);
 
         Digraph g = new Digraph(v);
-        RandomBag<Integer> rbA = new RandomBag<Integer>();
-        RandomBag<Integer> rbB = new RandomBag<Integer>();
-        for (int i = 0; i < v; i++) {
-            rbA.add(i);
-            rbB.add(i);
-        }
-
-        for (int i = 1; i <= e; i++) {
-            int a = rbA.iterator().next();
-            int b = rbB.iterator().next();
-            if (g.hasEdge(a, b) || a == b) {
-                rbA.add(a);
-                rbB.add(b);
-                i--;
-            }
-            g.addEdge(a, b);
+        Random rn = new Random();
+        for (int i = 0; i < e; i++) {
+            g.addEdge(rn.nextInt(v), rn.nextInt(v));
         }
     }
 }

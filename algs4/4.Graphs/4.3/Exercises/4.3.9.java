b1d815a78051568337fc1efc28a6e485bd5cc154
HuangStomach <nxmbest@qq.com>
2018-12-19 20:56:47 +0800

    习题至 4.3.9

---
+++ algs4/4.Graphs/4.3/Exercises/4.3.9.java
@@ -0,0 +1,67 @@
+import edu.princeton.cs.algs4.*;
+
+class EdgeWeightedGraph {
+    private final int V;
+    private int E;
+    private Bag<Edge>[] adj;
+
+    public EdgeWeightedGraph(int v) {
+        this.V = v;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new Bag<Edge>();
+        }
+    }
+    
+    public EdgeWeightedGraph(In in) {
+        this(in.readInt());
+        E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            int weight = in.readInt();
+            addEdge(new Edge(v, w, weight));
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(Edge e) {
+        int v = e.either;
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        E++;
+    }
+
+    public Iterable<Edge> adj(int v) {
+        return adj[v];
+    }
+
+    public Iterable<Edge> edges() {
+        Bag<Edge> b = new Bag<Edge>();
+        for (int v = 0; v < V; v++) {
+            for (Edge e: adj[v]) {
+                if (e.other(v) > v) b.add(e);
+            }
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        MST mst = new MST(G);
+        for (Edge e: mst.edges()) {
+            System.out.println(e);
+        }
+        System.out.println(mst.weight());
+    }
+}

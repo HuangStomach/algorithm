2167b0cf072c703d34532cb9fd7d8a400d73e0c5
HuangStomach <nxmbest@qq.com>
2019-01-05 23:43:57 +0800

    习题至 4.4.6

--- algs4/4.Graphs/4.4/Exercises/4.4.6.java
+++ algs4/4.Graphs/4.4/Exercises/4.4.6.java
@@ -1,5 +1,22 @@
 import edu.princeton.cs.algs4.*;
 
+class SPT {
+    public static void main(String[] args) {
+        int V = StdIn.readInt();
+        int E = StdIn.readInt();
+
+        EdgeWeightedDigraph positive = new EdgeWeightedDigraph(V);
+        for (int i = 0; i < E; i++) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            double weight = StdIn.readDouble();
+            positive.addEdge(new DirectedEdge(v, w, weight));
+        }
+
+        DijkstraSP sp = new DijkstraSP(positive, 2);
+    }
+}
+
 class DijkstraSP {
     private DirectedEdge[] edgeTo;
     private double[] distTo;
@@ -10,7 +27,7 @@ class DijkstraSP {
         distTo = new double[G.V()];
         pq = new IndexMinPQ<Double>(G.V());
 
-        for (int v = 0; v < G.V(); v++) {
+        for (int v = 1; v < G.V(); v++) {
             distTo[v] = Double.POSITIVE_INFINITY;
         }
         distTo[s] = 0.0;
@@ -25,7 +42,12 @@ class DijkstraSP {
             if (distTo[w] > distTo[v] + e.weight()) {
                 distTo[w] = distTo[v] + e.weight();
                 edgeTo[w] = e;
-                if (pq.contains(w)) pq.change(w, distTo[w]);
+                System.out.printf("distTo %d:", w);
+                for (DirectedEdge edge: pathTo(w)) {
+                    System.out.printf(" → [%d %d %.2f]", edge.from(), edge.to(), edge.weight());
+                }
+                System.out.println();
+                if (pq.contains(w)) pq.decreaseKey(w, distTo[w]);
                 else pq.insert(w, distTo[w]);
             }
         }

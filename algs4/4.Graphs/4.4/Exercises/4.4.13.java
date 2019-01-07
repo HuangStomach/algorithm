b13a705193a17ac490818bfbef7409c2e50ca2bf
HuangStomach <nxmbest@qq.com>
2019-01-07 21:55:32 +0800

    习题至 4.4.13

---
+++ algs4/4.Graphs/4.4/Exercises/4.4.13.java
@@ -0,0 +1,73 @@
+import edu.princeton.cs.algs4.*;
+
+class SPT {
+    public static void main(String[] args) {
+        int V = StdIn.readInt();
+        int E = StdIn.readInt();
+
+        EdgeWeightedDigraph positive = new EdgeWeightedDigraph(V);
+        for (int i = 0; i < E; i++) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            double weight = StdIn.readDouble();
+            positive.addEdge(new DirectedEdge(v, w, weight));
+        }
+
+        DijkstraSP sp = new DijkstraSP(positive, 0);
+    }
+}
+
+class DijkstraSP {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<Double> pq;
+
+    public DijkstraSP(EdgeWeightedDigraph G, int s) {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+        pq = new IndexMinPQ<Double>(G.V());
+
+        for (int v = 1; v < G.V(); v++) {
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        distTo[s] = 0.0;
+
+        pq.insert(s, 0.0);
+        while(!pq.isEmpty()) relax(G, pq.delMin());
+    }
+
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e: G.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (pq.contains(w)) pq.decreaseKey(w, distTo[w]);
+                else pq.insert(w, distTo[w]);
+            }
+        }
+
+        System.out.println("   edgeTo[] distTo[]");
+        for (int w = 0; w < G.V(); w++) {
+            if (edgeTo[w] == null) continue;
+            System.out.printf("%d   %d → %d    %.2f\n", w, edgeTo[w].from(), w, distTo[w]);
+        }
+    }
+
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.push(e);
+        }
+        return path;
+    }
+}

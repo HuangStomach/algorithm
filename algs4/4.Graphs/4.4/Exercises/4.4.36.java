cbc5273d3c5364e10c9294c6112984eddc49ca99
HuangStomach <nxmbest@qq.com>
2019-01-12 21:16:12 +0800

    习题至 4.4.36

--- algs4/4.Graphs/4.4/Exercises/4.4.36.java
+++ algs4/4.Graphs/4.4/Exercises/4.4.36.java
@@ -5,45 +5,32 @@ import edu.princeton.cs.algs4.*;
 class DijkstraSP {
     private DirectedEdge[] edgeTo;
     private double[] distTo;
-    private boolean[] marked;
-    private MinPQ<DirectedEdge> pq;
-    
-    private class ByDistanceFromSource implements Comparator<DirectedEdge> {
-        public int compare(DirectedEdge e, DirectedEdge f) {
-            double dist1 = distTo[e.from()] + e.weight();
-            double dist2 = distTo[f.from()] + f.weight();
-            return Double.compare(dist1, dist2);
-        }
-    }
+    private IndexMinPQ<Double> pq;
+    private double d;
 
-    public DijkstraSP(EdgeWeightedDigraph G, int s) {
+    public DijkstraSP(EdgeWeightedDigraph G, int s, double d) {
         edgeTo = new DirectedEdge[G.V()];
         distTo = new double[G.V()];
-        marked = new boolean[G.V()];
-        pq = new MinPQ<DirectedEdge>(new ByDistanceFromSource());
+        pq = new IndexMinPQ<Double>(G.V());
+        this.d = d;
 
         for (int v = 0; v < G.V(); v++) {
-            distTo[v] = Double.POSITIVE_INFINITY;
+            distTo[v] = d;
         }
         distTo[s] = 0.0;
 
-        for (DirectedEdge e: G.edges()) {
-            pq.insert(e);
-        }
-
-        while (!pq.isEmpty()) {
-            DirectedEdge e = pq.delMin();
-            int w = e.to();
-            relax(G, w);
-        }
+        pq.insert(s, 0.0);
+        while(!pq.isEmpty()) relax(G, pq.delMin());
     }
 
     private void relax(EdgeWeightedDigraph G, int v) {
-        for (DirectedEdge e : G.adj(v)) {
+        for (DirectedEdge e: G.adj(v)) {
             int w = e.to();
             if (distTo[w] > distTo[v] + e.weight()) {
                 distTo[w] = distTo[v] + e.weight();
                 edgeTo[w] = e;
+                if (pq.contains(w)) pq.decreaseKey(w, distTo[w]);
+                else pq.insert(w, distTo[w]);
             }
         }
     }
@@ -53,7 +40,7 @@ class DijkstraSP {
     }
 
     public boolean hasPathTo(int v) {
-        return distTo[v] < Double.POSITIVE_INFINITY;
+        return distTo[v] < d;
     }
 
     public Iterable<DirectedEdge> pathTo(int v) {
@@ -79,6 +66,7 @@ class DijkstraSP {
 
         DijkstraSP sp = new DijkstraSP(G, 0);
         for (int v = 0; v < V; v++) {
+            if (!sp.hasPathTo(v)) continue;
             System.out.print(v);
             for (DirectedEdge e: sp.pathTo(v)) {
                 System.out.printf(" → [%d %d %.2f]", e.from(), e.to(), e.weight());

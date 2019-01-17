d3cb6531c5ea4713d5cd61021adf64910c2a9ff9
HuangStomach <nxmbest@qq.com>
2019-01-17 23:06:23 +0800

    continue 5.1

--- algs4/2.Sort/2.1/Insertion.java
+++ algs4/2.Sort/2.1/Insertion.java
@@ -1,4 +1,4 @@
-public class Insertion extends Example {
+class Insertion extends Example {
     public static void sort(Comparable[] a) {
         for (int i = 1; i < a.length; i++) {
             for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {

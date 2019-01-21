6590d910c06c2458d5180a026e6755820a9886d0
HuangStomach <nxmbest@qq.com>
2019-01-21 21:27:30 +0800

    习题至 5.1.5

---
+++ algs4/5.Strings/5.1/Exercises/5.1.5.java
@@ -0,0 +1,61 @@
+import edu.princeton.cs.algs4.*;
+
+class MSD {
+    private static int R = 256;
+    private static final int M = 15;
+    private static String[] aux;
+
+    private static int charAt(String s, int d) {
+        if (d < s.length()) return s.charAt(d);
+        else return -1;
+    }
+
+    public static void sort(String[] a) {
+        int N = a.length;
+        aux = new String[N];
+        sort(a, 0, N - 1, 0);
+    }
+
+    private static void sort(String[] a, int lo, int hi, int d) {
+        if (hi <= lo + M) {
+            Insertion.sort(a, lo, hi);
+
+            for (int i = 0; i < a.length; i++) {
+                System.out.print(a[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        int[] count = new int[R + 2];
+        for (int i = lo; i <= hi; i++) {
+            count[charAt(a[i], d) + 2]++;
+        }
+
+        for (int r = 0; r < R + 1; r++) {
+            count[r + 1] += count[r];
+        }
+
+        for (int i = lo; i <= hi; i++) {
+            aux[count[charAt(a[i], d) + 1]++] = a[i];
+        }
+
+        for (int i = lo; i < hi; i++) {
+            a[i] = aux[i - lo];
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+
+        for (int r = 0; r < R; r++) {
+            sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] list = {"now", "is", "the", "time", "for", "all", "good", "people", "to", "come", "to", "the", "aid", "of"};
+        sort(list);
+    }
+}

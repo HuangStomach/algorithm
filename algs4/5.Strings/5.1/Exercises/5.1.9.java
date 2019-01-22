4588693adbdb16dcb90112faf5ae5be9c8d046e2
HuangStomach <nxmbest@qq.com>
2019-01-22 22:05:37 +0800

    习题至 5.1.9

---
+++ algs4/5.Strings/5.1/Exercises/5.1.9.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.*;
+
+class LSD {
+    private static int charAt(String s, int d) {
+        if (d < s.length()) return s.charAt(d);
+        else return -1;
+    }
+
+    public static void sort(String[] a, int W) {
+        int N = a.length;
+        int R = 256;
+        String[] aux = new String[N];
+        for (int d = W - 1; d >= 0; d--) {
+            int[] count = new int[R + 1];
+            for (int i = 0; i < N; i++) {
+                count[charAt(a[i], d) + 1]++;
+            }
+
+            for (int r = 0; r < R; r++) {
+                count[r + 1] += count[r];
+            }
+
+            for (int i = 0; i < N; i++) {
+                aux[count[charAt(a[i], d)]++] = a[i];
+            }
+
+            for (int i = 0; i < N; i++) {
+                a[i] = aux[i];
+            }
+        }
+    }
+}

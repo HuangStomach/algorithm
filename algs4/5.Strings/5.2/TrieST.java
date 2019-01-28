e93463280b789bf6c615de38dd0db30b2f5baa39
HuangStomach <nxmbest@qq.com>
2019-01-28 20:40:32 +0800

    continue 5.2

--- algs4/5.Strings/5.2/TrieST.java
+++ algs4/5.Strings/5.2/TrieST.java
@@ -50,4 +50,22 @@ public class TrieST<Value> {
         }
         return cnt;
     }
+
+    public Iterable<String> keys() {
+        return keysWithPrefix("");
+    }
+
+    public Iterable<Stirng> keysWithPrefix(String pre) {
+        Queue<String> q = new Queue<String>();
+        collect(get(root, pre, 0), pre, q);
+        return q;
+    }
+
+    private void collect(Node x, String pre, Queue<String> q) {
+        if (x == null) return;
+        if (x.val != null) q.enqueue(pre);
+        for (char c = 0; c < R; c++) {
+            collect(x.next[c], pre + c, q);
+        }
+    }
 }

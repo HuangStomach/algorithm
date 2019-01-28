f615f62ce23cdb830f1f9ac0f6776c9a8276435e
HuangStomach <nxmbest@qq.com>
2019-01-28 20:58:05 +0800

    continue 5.2

--- algs4/5.Strings/5.2/TrieST.java
+++ algs4/5.Strings/5.2/TrieST.java
@@ -99,4 +99,23 @@ public class TrieST<Value> {
         char c = s.charAt(d);
         return search(x.next[c], s, d + 1, length);
     }
+
+    public void delete(String key) {
+        root = delete(root, key, 0);
+    }
+
+    private Node delete(Node x, String key, int d) {
+        if (x == null) return null;
+        if (d == key.length()) x.val = null;
+        else {
+            char c = key.charAt(d);
+            x.next[c] = delete(x.next[c], key, d + 1);
+        }
+
+        if (x.val != null) return x;
+        for (char c = 0; c < R; c++) {
+            if (x.next[c] != null) return x;
+        }
+        return null;
+    }
 }

0424c2cdce98ca2a70b1af3dfb6851e977eeb244
HuangStomach <nxmbest@qq.com>
2019-02-11 22:38:45 +0800

    continue 5.3

--- algs4/5.Strings/5.3/KMP.java
+++ algs4/5.Strings/5.3/KMP.java
@@ -28,4 +28,17 @@ class KMP {
         if (j == M) return i - M;
         else return N;
     }
+
+    public static void main(String[] args) {
+        String pat = args[0];
+        String txt = args[1];
+        KMP kmp = new KMP(pat);
+        System.out.println("text:    " + txt);
+        int offset = kmp.search(txt);
+        System.out.print("pattern: ");
+        for (int i = 0; i < offset; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(pat);
+    }
 }

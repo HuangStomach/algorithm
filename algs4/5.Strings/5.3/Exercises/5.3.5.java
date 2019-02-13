658c2745e166a55deaa7310b1c3f454d7f391b23
HuangStomach <nxmbest@qq.com>
2019-02-13 21:55:51 +0800

    习题至 5.3.5

---
+++ algs4/5.Strings/5.3/Exercises/5.3.5.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.*;
+
+class BruteForceRL {
+    private String pat;
+
+    BoyerMoore(String pat) {
+        this.pat = pat;
+    }
+
+    public int search(String txt) {
+        int N = txt.length();
+        int M = pat.length();
+        
+        for (int i = 0; i <= N - M; i++) {
+            for (int j = M - 1; j >= 0; j--) {
+                if (pat.charAt(j) != pat.charAt(i + j)) {
+                    break;
+                }
+            }
+
+            if (j == 0) return i;
+        }
+        return M;
+    }
+
+    public static void main(String[] args) {
+        String pat = args[0];
+        String txt = args[1];
+        BoyerMoore boyerMoore = new BoyerMoore(pat);
+        System.out.println("text:    " + txt);
+        int offset = boyerMoore.search(txt);
+        System.out.print("pattern: ");
+        for (int i = 0; i < offset; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(pat);
+    }
+}

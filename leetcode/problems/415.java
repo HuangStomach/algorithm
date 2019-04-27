d5ffe043d218cac028912a5bddedef09b24be0a5
HuangStomach <nxmbest@qq.com>
2019-04-27 21:57:12 +0800

    leetcode 415

---
+++ leetcode/problems/415.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+// 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        while (i >= 0 || j >= 0 || carry != 0) {
+            if (i >= 0) carry += num1.charAt(i--) - '0';
+            if (j >= 0) carry += num2.charAt(j--) - '0';
+            sb.append(carry % 10);
+            carry /= 10;
+        }
+        return sb.reverse().toString();
+    }
+}

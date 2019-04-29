af06f10a0cba1c4873ccd1ab9a24820bd74c3965
HuangStomach <nxmbest@qq.com>
2019-04-29 18:10:55 +0800

    leetcode 152 乘积最大子序列

--- leetcode/problems/152.java
+++ leetcode/problems/152.java
@@ -9,15 +9,14 @@ class Solution {
         int res = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] >= 0) {
-                max = Math.max(max * nums[i], nums[i]);
-                min = Math.min(min * nums[i], nums[i]);
-            }
-            else {
-                int max_temp = max;
-                max = Math.max(min * nums[i], nums[i]);
-                min = Math.min(max_temp * nums[i], nums[i]);
-            }
+            if (nums[i] < 0) { 
+                int tmp = max; 
+                max = min; 
+                min = tmp;
+            } 
+            
+            max = Math.max(max * nums[i], nums[i]);
+            min = Math.min(min * nums[i], nums[i]);
             res = Math.max(max, res);
         }
 

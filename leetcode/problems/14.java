31a4ea507b07417dc8c2ec2485ab7659fa1ac168
HuangStomach <nxmbest@qq.com>
2019-05-05 21:22:45 +0800

    leetcode 14 最长公共前缀

---
+++ leetcode/problems/14.java
@@ -0,0 +1,18 @@
+/**
+ * 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ */
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}

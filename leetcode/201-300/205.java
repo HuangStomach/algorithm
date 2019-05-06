12c8823dc45d587450624aaf6695a85d674e8cb3
HuangStomach <nxmbest@qq.com>
2019-05-06 22:25:56 +0800

    leetcode 205 同构字符串

---
+++ leetcode/201-300/205.java
@@ -0,0 +1,21 @@
+/**
+ * 同构字符串
+ * 给定两个字符串 s 和 t，判断它们是否是同构的。
+ * 如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+ * 所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+ */
+import java.util.HashMap;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))) {
+                if (map.containsValue(t.charAt(i))) return false;
+                map.put(s.charAt(i), t.charAt(i));
+            }
+            else if (map.get(s.charAt(i)) != t.charAt(i)) return false;
+        }
+        return true;
+    }
+}

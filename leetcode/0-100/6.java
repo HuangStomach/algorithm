11f0d34de5f5d2a16fa7e17c4e0eab99b49fff33
HuangStomach <nxmbest@qq.com>
2019-05-13 21:00:54 +0800

    leetcode 6 Z 字形变换

---
+++ leetcode/0-100/6.java
@@ -0,0 +1,23 @@
+/**
+ * Z 字形变换
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ */
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret.append(s.charAt(j + cycleLen - i));
+            }
+        }
+        return ret.toString();
+    }
+}

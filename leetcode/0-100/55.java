cbe2920edb90b7e9a946dabfc370580877d0a5a7
HuangStomach <nxmbest@qq.com>
2019-05-23 07:22:35 +0900

    leetcode 55  Jump Game

---
+++ leetcode/0-100/55.java
@@ -0,0 +1,17 @@
+/**
+ * Jump Game
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ * Each element in the array represents your maximum jump length at that position.
+ * Determine if you are able to reach the last index.
+ */
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}

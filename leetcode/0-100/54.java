5a5a03742ad244a3f08f9ab2d9145c63b1b2d3f9
HuangStomach <nxmbest@qq.com>
2019-05-24 22:11:13 +0900

    leetcode 54 螺旋矩阵

--- leetcode/0-100/54.java
+++ leetcode/0-100/54.java
@@ -1,6 +1,6 @@
 /**
- * Spiral Matrix
- * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ * 螺旋矩阵
+ * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
  */
 class Solution {
     public List < Integer > spiralOrder(int[][] matrix) {

b0485b4fbba828f8a0efafc6e01dd702e45b4d0e
HuangStomach <nxmbest@qq.com>
2019-06-04 21:09:48 +0800

    leetcode 96 不同的二叉搜索树

---
+++ leetcode/0-100/96.java
@@ -0,0 +1,19 @@
+/**
+ * 不同的二叉搜索树
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ */
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
+}

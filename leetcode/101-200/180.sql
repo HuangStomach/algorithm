4098dc16391c356b0c997ec2c2993631e8babfe3
HuangStomach <nxmbest@qq.com>
2019-06-11 21:59:15 +0800

    leetcode 180 连续出现的数字

---
+++ leetcode/101-200/180.sql
@@ -0,0 +1,12 @@
+# 连续出现的数字
+# 编写一个 SQL 查询，查找所有至少连续出现三次的数字。
+select distinct Num as ConsecutiveNums
+from (
+  select Num, 
+    case 
+      when @prev = Num then @count := @count + 1
+      when (@prev := Num) is not null then @count := 1
+    end as CNT
+  from Logs, (select @prev := null,@count := null) as t
+) as temp
+where temp.CNT >= 3

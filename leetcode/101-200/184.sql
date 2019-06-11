d5517331ff69e71391ec02295d813ebe639e7501
HuangStomach <nxmbest@qq.com>
2019-06-11 22:13:17 +0800

    leetcode 184 部门工资最高的员工

---
+++ leetcode/101-200/184.sql
@@ -0,0 +1,12 @@
+# 部门工资最高的员工
+# 编写一个 SQL 查询，找出每个部门工资最高的员工。例如，根据上述给定的表格，Max 在 IT 部门有最高工资，Henry 在 Sales 部门有最高工资。
+select 
+    d.Name as Department,
+    e.Name as Employee,
+    e.Salary 
+from 
+    Employee e,Department d 
+where
+    e.DepartmentId=d.id 
+    and
+    (e.Salary,e.DepartmentId) in (select max(Salary),DepartmentId from Employee group by DepartmentId);

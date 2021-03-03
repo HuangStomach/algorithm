17dc1828e3f714edc61089173d7337931f07b12d
HuangStomach <nxmbest@qq.com>
2021-03-03 12:19:35 +0800

    541. 反转字符串 II

---
+++ leetcode/501-600/541.cpp
@@ -0,0 +1,21 @@
+/** 
+ * 541. 反转字符串 II
+ * 给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。
+ * 如果剩余字符少于 k 个，则将剩余字符全部反转。
+ * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ */
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += 2 * k) {
+            if (i + k > s.size()) reverse(s.begin() + i, s.end());
+            else reverse(s.begin() + i, s.begin() + i + k);
+        }
+        return s;
+    }
+};
